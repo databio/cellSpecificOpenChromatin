3ba0289a8275eae4cb91bb01a392b8ef3235fd1f
Kristyna Kupkova <kristynakupkova@gmail.com>
2020-10-13 15:30:15 -0400

    output is now merged bigWig, not bedGraph

--- dataDownloadAndPreprocess/mergeBigWig.sh
+++ dataDownloadAndPreprocess/mergeBigWig.sh
@@ -4,7 +4,11 @@
 # a bigWig named according to the directory - this produces bedGraph
 # move the files to directory provided
 echo -e What is the full path to the folder where outputs should be stored?
-read bedGraphFolder
+read bigWigFolder
+
+echo -e What is the full path to the file with chromosome sizes?
+read chromSize
+
 
 for f in *; do
     if [ -d "$f" ]; then
@@ -13,9 +17,15 @@ for f in *; do
         
         cd $f
         
+        #merge the bigWig files
         bigWigMerge *.bigWig $f.bedGraph
         
-        mv $f.bedGraph $bedGraphFolder
+        # convert bedGraph to bigWig
+        bedGraphToBigWig $f.bedGraph $chromSize $f.bw
+        
+        # move the merged bigWig to designated folder and remove bedGraph file
+        mv $f.bw $bigWigFolder
+        rm $f.bedGraph
         
         cd ..
     fi
